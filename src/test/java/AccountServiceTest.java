d7d7c0a2564c4a08a0dd92f84666fd07e70f6236
mouadHILLALI <mauadtoto001@gmail.com>
2025-01-30 02:59:43 +0100

    added test to test deposit behavior

--- src/test/java/AccountServiceTest.java
+++ src/test/java/AccountServiceTest.java
@@ -1,20 +1,22 @@
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
+import com.skypay.model.Account;
 import com.skypay.service.Impl.AccountService;
+import com.skypay.service.Impl.AccountServiceImpl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class AccountServiceTest {
     
     @Test
     public void deposit(){
-        AccountService accountService = new AccountService();
-        accountService.deposit();
-        String statement = accountService.printStatement();
-        String expectedStatement =
-        "Date || Amount || Balance\n" +
-        "10-01-2012 || 1000 || 1000\n";
-         assertEquals(expectedStatement);
+        Account mockAccount = mock(Account.class);
+        AccountServiceImpl accountService = new AccountServiceImpl(mockAccount);
+        accountService.deposit(300);
+        verify(mockAccount).deposit(300);
     }
 
 }
