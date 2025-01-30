278f881287b08a86dda2ab8c1990dd251071f3e8
mouadHILLALI <mauadtoto001@gmail.com>
2025-01-30 03:29:39 +0100

    working on implementing the deposit method

--- src/main/java/com/skypay/service/AccountService.java
+++ src/main/java/com/skypay/service/AccountService.java
@@ -1,7 +1,12 @@
 package com.skypay.service;
 
+import java.util.List;
+
+import com.skypay.model.Transaction;
+
 public interface AccountService {
     void deposit(int amount);
     void withdraw(int amount);
     void printStatement();
+    List<Transaction> addTransaction(Transaction transaction);
 }
--- src/main/java/com/skypay/service/Impl/AccountServiceImpl.java
+++ src/main/java/com/skypay/service/Impl/AccountServiceImpl.java
@@ -1,11 +1,26 @@
 package com.skypay.service.Impl;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.skypay.model.Account;
+import com.skypay.model.Transaction;
 import com.skypay.service.AccountService;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
+
+    private final Account account;
     
     public void deposit(int amount){
-        
+        try {
+            account.setBalance(amount);
+            account.setTransactions(this.addTransactions(new Transaction(LocalDate.now() , amount)));
+        } catch (Exception e) {
+            
+        }
     }
 
     public void withdraw(int amount){
@@ -13,6 +28,15 @@ public class AccountServiceImpl implements AccountService {
     }
 
     public void printStatement(){
-        
+
+    }
+    
+    public List<Transaction> addTransactions(Transaction transaction){
+        try {
+            List<Transaction> transactions = account.getTransactions().add(transaction);
+            return transactions;
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
